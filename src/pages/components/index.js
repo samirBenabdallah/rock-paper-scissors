48e31761e2fa6ece7b801d245484400e92b99e85
samirBenabdallah <155623465+samirBenabdallah@users.noreply.github.com>
2024-03-28 21:10:48 +0100

    Delete src directory

--- src/pages/components/index.js
+++
@@ -1,6 +0,0 @@
-import Header from "./header/Header";
-import Button from "./button/Button";
-import Rules from "./rules/Rules";
-import Element from "./elements/Element";
-
-export { Header, Button, Rules, Element };
--- src/rtk/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import score from "./score";
-const store = configureStore({
-  reducer: {
-    score,
-  },
-});
-export default store;
